e842357a11bb74088c4cacb2091d3790d3295b5e
mwikalikyalo <winniemwikali07@gmail.com>
2022-06-07 22:49:18 +0300

    Add final text to be printed after the game

--- generate.py
+++ generate.py
@@ -17,14 +17,18 @@ def main():
     print(name +",Your age is " + as_string)
 
   #when you were born
-  q2= input("Which year were you born?")
-  year_born= int(q2)
-  for year in q2:
-    print(year_born)
-  Break
-  
-  #turning 80
-  q1= prompt("Would you like to find out when you will be 80?")
+    q1= input("Which year were you born? ")
+    for year_born in q1:
+      if q1 >= 0:
+        print(year_born)
+
+    #turning 80
+    q2= prompt("Would you like to find out when you will be 80?")
+    prompt=input(str())
+    if q2== "Yes" or 'yes':
+        print(year_born+80)
+    else:
+        print("Thank you for playing the birthday game.")
 
 if __name__ == '__main__':
     main()
